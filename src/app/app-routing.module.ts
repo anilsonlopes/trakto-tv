9cd84a94fb7d5cdc8f62e8c33a4ea9d6d4318a28
Anilson Lopes <nissobmx@gmail.com>
2023-04-23 12:20:35 -0300

    :sparkles: adiciona roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MaterialComponent } from './material/material.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialTodosComponent } from './material-todos/material-todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'material',
+    component: MaterialComponent,
+  },
+  {
+    path: 'material-todos',
+    component: MaterialTodosComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
