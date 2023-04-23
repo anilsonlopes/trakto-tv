953e040c2419d7e286c97a09f9217107733a61ed
Anilson Lopes <nissobmx@gmail.com>
2023-04-23 18:09:48 -0300

    :sparkles: declara components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { MaterialComponent } from './material/material.component';
+import { MaterialTodosComponent } from './material-todos/material-todos.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    MaterialComponent,
+    MaterialTodosComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
